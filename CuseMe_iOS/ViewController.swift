711b189c41614d70f258c56d5bcfa93009f4cb01
wookeon <creator_wookeon@naver.com>
2019-12-24 19:31:36 +0900

    프로젝트 세팅

---
+++ CuseMe_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CuseMe_iOS
+//
+//  Created by wookeon on 2019/12/24.
+//  Copyright © 2019 cuseme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CuseMe_iOS' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CuseMe_iOS
+  pod 'Kingfisher'
+  pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'Alamofire'
+end
