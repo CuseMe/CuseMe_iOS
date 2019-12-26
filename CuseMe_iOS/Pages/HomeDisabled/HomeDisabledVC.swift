cada331bc648710f4b8fe150199226c0c387f530
신유진 <shinnew320@gmail.com>
2019-12-26 15:53:09 +0900

    commit HomeDisbled

---
+++ CuseMe_iOS/Pages/HomeDisabled/HomeDisabledVC.swift
@@ -0,0 +1,13 @@
+//
+//  HomeDisabledVC.swift
+//  CuseMe_iOS
+//
+//  Created by Yujin Shin on 2019/12/26.
+//  Copyright © 2019 cuseme. All rights reserved.
+//
+
+import UIKit
+
+class HomeDisabledVC: UICollectionViewCell {
+    
+}
