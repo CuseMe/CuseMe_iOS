88d1ecf6f52fb64d48127ad25f821b018d5ba472
wookeon <creator_wookeon@naver.com>
2019-12-30 19:22:49 +0900

    commit custom UITabBarController

---
+++ CuseMe_iOS/Utils/UIColor+Extensions.swift
@@ -0,0 +1,40 @@
+//
+//  UIColor+Extensions.swift
+//  CuseMe_iOS
+//
+//  Created by wookeon on 2019/12/30.
+//  Copyright © 2019 cuseme. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+  @nonobjc class var mainpink: UIColor {
+    return UIColor(red: 251.0 / 255.0, green: 109.0 / 255.0, blue: 106.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var veryLightPinkTwo: UIColor {
+    return UIColor(red: 254.0 / 255.0, green: 218.0 / 255.0, blue: 217.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var seafoamBlue: UIColor {
+    return UIColor(red: 129.0 / 255.0, green: 214.0 / 255.0, blue: 212.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var greyishBrown: UIColor {
+    return UIColor(white: 74.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var brownGrey: UIColor {
+    return UIColor(white: 180.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var veryLightGray: UIColor {
+    return UIColor(white: 186.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var greyishBrownTwo: UIColor {
+    return UIColor(white: 75.0 / 255.0, alpha: 1.0)
+  }
+}
