89826b57c38faec186ae03f5d7ac3534438315ab
wookeon <creator_wookeon@naver.com>
2020-01-03 19:18:37 +0900

    commit foldering protocol and change base URL

--- CuseMe_iOS/Domains/Protocols/APIManager.swift
+++ CuseMe_iOS/Domains/Protocols/APIManager.swift
@@ -10,6 +10,6 @@ protocol APIManager {}
 
 extension APIManager {
     public static func setURL(_ path: String) -> String {
-        return "http://15.165.108.23:3000" + path
+        return "http://13.125.41.98:3000" + path
     }
 }
